1a861ab81414bbfe9b8506be946b63700ab42285
Shirendev08 <shirendev.07@gmail.com>
2023-05-29 14:58:21 +0800

    login service

--- frontend/src/screen/Register.js
+++ frontend/src/screen/Register.js
@@ -1,15 +1,39 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, Button } from 'react-native';
+import axios from 'axios';
+import { useNavigation } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const LoginScreen = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleLogin = () => {
-    // Perform login logic here
-    console.log('Username:', username);
-    console.log('Password:', password);
+  const navigation = useNavigation();
+
+
+
+  const handleLogin = () => { 
+  
+  const loginData = {
+    username: username,
+    password: password,
   };
+
+  axios.post('http://127.0.0.1:8000/login', loginData)
+    .then(response => {
+      if (response.data === 'okey') {
+        console.log('Login successful');
+        navigation.navigate('Home');
+        
+      } else {
+        console.log('Login failed');
+      }
+    })
+    .catch(error => {
+      console.error(error);
+    });
+};
+
 
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
