ce67f125d614d0f3179c35d236576ce0cfb6485d
GanzorigUlziibat <94421033+GanzorigUlziibat@users.noreply.github.com>
2023-05-31 12:34:45 +0800

    Login template

--- frontend/src/screen/Register.js
+++ frontend/src/screen/Register.js
@@ -1,65 +1,132 @@
 // LoginScreen.js
-import React, { useState, useContext } from 'react';
-import { View, Text, TextInput, Button } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import UserContext from '../UserContext';
-import axios from 'axios';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useState, useContext } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Pressable,
+  Platform,
+  StatusBar,
+} from "react-native";
+// import { useNavigation } from "@react-navigation/native";
+import UserContext from "../UserContext";
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const LoginScreen = () => {
+const LoginScreen = ({ navigation }) => {
   const { updateUser } = useContext(UserContext);
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const navigation = useNavigation();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  // const navigation = useNavigation();
 
   const handleLogin = () => {
     const loginData = {
       username: username,
       password: password,
     };
-  
-    axios.post('http://arrive.mandakh.org/login', loginData)
-      .then(response => {
-        if (response.data && response.data.data && response.data.data.length > 0) {
+
+    axios
+      .post("http://arrive.mandakh.org/login", loginData)
+      .then((response) => {
+        if (
+          response.data &&
+          response.data.data &&
+          response.data.data.length > 0
+        ) {
           const userdata = response.data.data[0];
-          AsyncStorage.setItem('userData', JSON.stringify({ userdata }))
-          .then(() => {
-            updateUser(userdata); 
-            navigation.navigate('Home');
-          })
-          .catch(error => {
-            console.error('Error saving user data:', error);
-          });
-          
+          AsyncStorage.setItem("userData", JSON.stringify({ userdata }))
+            .then(() => {
+              updateUser(userdata);
+              navigation.navigate("Home");
+            })
+            .catch((error) => {
+              console.error("Error saving user data:", error);
+            });
         } else {
-          console.error('Invalid response format111');
+          console.error("Invalid response format111");
         }
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error);
       });
   };
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      {/* Your login form UI */}
-      <Text style={{ fontSize: 24, marginBottom: 20 }}>Нэвтрэх</Text>
-      <TextInput
-        style={{ width: '50%', height: 40, borderColor: 'gray', borderWidth: 1, marginBottom: 10, padding: 10 }}
-        placeholder="Оюутны код"
-        value={username}
-        onChangeText={setUsername}
-      />
-      <TextInput
-        style={{ width: '50%', height: 40, borderColor: 'gray', borderWidth: 1, marginBottom: 10, padding: 10 }}
-        placeholder="Нууц үг"
-        secureTextEntry
-        value={password}
-        onChangeText={setPassword}
-      />
-      <Button title="Нэвтрэх" onPress={handleLogin} />
-    </View>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+      }}
+    >
+      <ScrollView>
+        <View
+          style={{
+            height: 500,
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          {/* Your login form UI */}
+          <Text style={{ fontSize: 24, marginBottom: 20 }}>Нэвтрэх</Text>
+          <TextInput
+            style={{
+              width: "50%",
+              height: 40,
+              borderColor: "gray",
+              borderWidth: 1,
+              marginBottom: 10,
+              padding: 10,
+            }}
+            placeholder="Оюутны код"
+            value={username}
+            onChangeText={setUsername}
+          />
+          <TextInput
+            style={{
+              width: "50%",
+              height: 40,
+              borderColor: "gray",
+              borderWidth: 1,
+              marginBottom: 10,
+              padding: 10,
+            }}
+            placeholder="Нууц үг"
+            secureTextEntry
+            value={password}
+            onChangeText={setPassword}
+          />
+
+          <Pressable style={styles.button} onPress={handleLogin}>
+            <Text style={styles.text}>Нэвтрэх</Text>
+          </Pressable>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: "black",
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: "bold",
+    letterSpacing: 0.25,
+    color: "white",
+  },
+});
 
 export default LoginScreen;
